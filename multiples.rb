53b06ffed752d27f6765fe90707af1010cb56b2b
Matthew Lee <matthewjlee256@gmail.com>
2016-08-13 16:03:06 +0900

    added description of problem

--- multiples.rb
+++ multiples.rb
@@ -1,3 +1,9 @@
+=begin
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+=end
+
 sum = 0
 
 for i in 1..1000 do
@@ -6,7 +12,7 @@ for i in 1..1000 do
 		sum += i
 	end
 end
-#have to put end on every single if statement, for statement, etc. 
+#reminder that you have to put end on every single if statement, for statement, etc. 
 
 puts sum
 
