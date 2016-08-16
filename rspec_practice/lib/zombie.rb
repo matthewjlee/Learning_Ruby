2a6558278bf206e5d49f2d68c9eab1eb8d318ec3
Matthew Lee <matthewjlee256@gmail.com>
2016-08-16 15:17:36 +0900

    rest of the exercise

--- rspec_practice/lib/zombie.rb
+++ rspec_practice/lib/zombie.rb
@@ -1,9 +1,16 @@
 class Zombie
-	attr_accessor :name, :brains
+	attr_accessor :name, :brains, :alive, :height
 	#same thing as a setter from Java.
 
 	def initialize
 		@name = 'Ash'
-		@brains = 0;
+		@brains = 0
+		@alive = false
+		@height = 4
 	end
+
+	def hungry?
+		true
+	end
+	#question mark means that it's a predicate. 
 end
--- rspec_practice/spec/lib/zombie_spec.rb
+++ rspec_practice/spec/lib/zombie_spec.rb
@@ -5,7 +5,7 @@ describe Zombie do
 	it "is named Ash" do #it is part of rspec framework. Used to encourage user to describe test.
 		zombie = Zombie.new
 		zombie.name.should == 'Ash'
-		this is an expectation. Same thing as an assert statement. Reads more like plain English.
+		#this is an expectation. Same thing as an assert statement. Reads more like plain English.
 	end
 
 	it "has no brains" do
@@ -13,6 +13,31 @@ describe Zombie do
 		zombie.brains.should < 1
 		#should is a modifier. The < symbol is a matcher.
 	end
-end
 
+	it "is alive" do
+		zombie = Zombie.new
+		#zombie.alive.should == false
+		zombie.alive.should be false
+		#different syntax using be structure
+	end
+
+	it "is shorter than 5 feet" do
+		zombie = Zombie.new
+		zombie.height.should < 5
+	end
+
+	it "is hungry" do
+		zombie = Zombie.new
+		#zombie.hungry?.should be true
+		zombie.should be_hungry
+		#can use this structure which reads better.
+	end
+	#can use shuold be structure to test this def
+
+	it "is named Ash" #this is a pending example.
+	xit "is named Ash" do
+	end					#another pending example
+	it "is named Ash" do
+		pending
+	end					#another pending example
 end
